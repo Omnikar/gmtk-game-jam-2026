faecf1da0c5e7ca44b27a7dea66978ed7294e055
Omnikar <omnikar5@gmail.com>
2026-07-23 09:45:40 -0400

    Add top-down player movement

---
+++ scripts/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@export var move_speed: float = 80.0
+
+
+func _ready() -> void:
+	pass
+
+
+func _physics_process(_delta: float) -> void:
+	var motion = Vector2(
+		float(Input.is_action_pressed("walk_right")) - float(Input.is_action_pressed("walk_left")),
+		float(Input.is_action_pressed("walk_down")) - float(Input.is_action_pressed("walk_up")),
+	)
+
+	# might not actually want this, since motion speed isn't a critical gameplay
+	# element and the world is going to be grid-organized
+	motion = motion.normalized()
+
+	velocity = move_speed * motion
+
+	move_and_slide()
