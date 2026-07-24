98d9ef11e98d392e8966c61d8e787f27aba6d7f4
Omnikar <omnikar5@gmail.com>
2026-07-24 09:38:00 -0400

    Characters yippee

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var move_speed: float = 80.0
 
+var facing_direction: String = "down"
+
 
 func _ready() -> void:
 	pass
@@ -23,8 +25,27 @@ func _physics_process(_delta: float) -> void:
 		# element and the world is going to be grid-organized
 		motion = motion.normalized()
 
+		if motion.y < 0:
+			facing_direction = "up"
+		elif velocity.y > 0:
+			facing_direction = "down"
+		elif velocity.x < 0:
+			facing_direction = "left"
+		elif velocity.x > 0:
+			facing_direction = "right"
+
 		velocity = move_speed * motion
 	else:
 		velocity = Vector2.ZERO
+
+	var status: String
+	if velocity != Vector2.ZERO:
+		status = "walk"
+	else:
+		status = "stand"
+
+	var animation_name = "%s_%s" % [facing_direction, status]
+
+	$AnimatedSprite2D.play(animation_name)
 
 	move_and_slide()
