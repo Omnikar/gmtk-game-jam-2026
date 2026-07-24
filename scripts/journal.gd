5746eb4332926594283524f25902be5f0a8d46f9
Omnikar <omnikar5@gmail.com>
2026-07-24 14:44:23 -0400

    WIP evidence presenting work

--- scripts/journal.gd
+++ scripts/journal.gd
@@ -5,6 +5,7 @@ extends Control
 var added_evidence: Dictionary[String, bool] = {}
 
 var present_mode: bool = false
+var present_target: String = ""
 var selected_evidence: Dictionary[String, bool] = {}
 
 
@@ -35,6 +36,7 @@ func _on_dialogic_signal(arg: Dictionary) -> void:
 		scene_instance.evidence_clicked.connect(_on_evidence_clicked)
 		vbox.add_child(scene_instance)
 	elif present_character != null:
+		present_target = present_character
 		start_present_mode()
 
 
@@ -44,16 +46,29 @@ func _on_evidence_clicked(tag: String) -> void:
 		for entry in $TabContainer/Evidence/MarginContainer/VBoxContainer.get_children():
 			if entry is EvidenceEntry and entry.tag == tag:
 				entry.selected = selected_evidence[tag]
+		var selected_count = 0
+		for selected in selected_evidence.values():
+			if selected:
+				selected_count += 1
+		var message = "%d items selected" % selected_count
+		$SelectedCount.text = message
 
 
 func start_present_mode() -> void:
 	present_mode = true
 	visible = true
+	$SelectedCount.visible = true
 	selected_evidence = {}
 
 
 func end_present_mode() -> void:
 	present_mode = false
+	$SelectedCount.visible = false
 	for entry in $TabContainer/Evidence/MarginContainer/VBoxContainer.get_children():
 		if entry is EvidenceEntry:
 			entry.selected = false
+
+
+func _on_present_button_pressed() -> void:
+	end_present_mode()
+	pass  # Replace with function body.
