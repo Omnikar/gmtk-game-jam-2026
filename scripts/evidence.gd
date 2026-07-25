c3ed4542eb2f43266fba748a644a41aaba331213
Omnikar <omnikar5@gmail.com>
2026-07-25 19:02:24 -0400

    Add evidence and stuff

--- scripts/evidence.gd
+++ scripts/evidence.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 @export var timeline: DialogicTimeline
+@export var reusable: bool = false
+
+var used = false
 
 
 func _input(event: InputEvent) -> void:
@@ -8,5 +11,7 @@ func _input(event: InputEvent) -> void:
 		event.is_action_pressed("interact")
 		and Dialogic.current_timeline == null
 		and has_overlapping_bodies()
+		and (reusable or not used)
 	):
+		used = true
 		Dialogic.start(timeline)
